7717dff84a8ddb64ef40dd1a2c30a4eea4a68674
bozhko-victor <bozhko.v.n@yandex.ru>
2021-12-14 19:12:39 +0300

    added SearchItemTests

--- src/test/java/cloud/autotests/tests/SearchItemTests.java
+++ src/test/java/cloud/autotests/tests/SearchItemTests.java
@@ -1,49 +1,35 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.config.App;
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 @Feature("Search")
 public class SearchItemTests extends TestBase {
 
     @Test
-    @AllureId("8159")
+    @AllureId("6160")
     @Description("Soon to be implemented")
-    @DisplayName("Successful search items ")
-    void successfulAuthorizationTest() {
-        step("Open main page", () -> {
-            open("");
-            $("#dropdown_login_form").click();
-            switchTo().window(1);
+    @DisplayName("Successful product search by name")
+    void successfulSearchItemTest() {
+        step("Open main page", () ->
+                open("")
+        );
+
+        step("Enter the product name into the search field", () -> {
+            $("#search-query-top").setValue("iphone 13").pressEnter();
         });
 
-        step("Set login", () -> {
-            $("[name='login[email]']").setValue(App.config.userLogin());
+        step("Сheck the search results contain the product you are looking for", () -> {
+            $$(".b-stories_content_search-results > li").shouldHave(sizeGreaterThan(0));
         });
 
-        step("Set password", () -> {
-            $("[name='login[password]']").setValue(App.config.userPassword());
-        });
-
-        step("Press ENTER", () -> {
-            $("[name='submit']").pressEnter();
-        });
-
-        step("Check successful autorization", () -> {
-            switchTo().window(0);
-//            $("#user_menu_container").shouldHave(Condition.text(App.config.userLogin()));
-            $(".b-link_icon_user-profile").click();
-            $("#user_menu_container").shouldHave(Condition.text(App.config.userLogin()));
-        });
     }
 
 }
